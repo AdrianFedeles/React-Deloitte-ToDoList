911b27d342c587cda547de199700c4271f662df6
FedelesAdrian <afedeles@deloittece.com>
2023-10-12 19:52:00 +0300

    first message to this ToDo List using React Js, TypeScript, Scss,

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App '>
+			<section className='container-wrapper'>
+				<div className='container-title'>
+					<h1 className='title-toDo'>To Do List</h1>
+				</div>
+			</section>
+			{/* <component/> */}
+			{/* <component/> */}
+			{/* <component/> */}
+		</div>
+	);
 }
 
 export default App;
